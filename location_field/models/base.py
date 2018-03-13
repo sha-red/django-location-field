565f91418b38e6a5024df5ef2d7430435dbc262d
Erik Stein <erik@classlibrary.net>
2018-03-13 04:25:08 +0100

    Improved field initialization.

--- location_field/models/base.py
+++ location_field/models/base.py
@@ -1,11 +1,15 @@
 class BaseLocationField(object):
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         self._based_fields = kwargs.pop('based_fields', [])
         self._zoom = kwargs.pop('zoom', 2)
+        self._suffix = kwargs.pop('suffix', None)
+        self._initial = kwargs.pop('initial', '')
+        super(BaseLocationField, self).__init__(*args, **kwargs)
 
     def formfield(self, **kwargs):
         return super(BaseLocationField, self).formfield(
             form_class=self.formfield_class,
             based_fields=self._based_fields,
             zoom=self._zoom,
+            initial=self._initial,
             **kwargs)
--- location_field/models/plain.py
+++ location_field/models/plain.py
@@ -7,11 +7,6 @@ from location_field.models.base import BaseLocationField
 class PlainLocationField(BaseLocationField, CharField):
     formfield_class = forms.PlainLocationField
 
-    def __init__(self, max_length=63, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('max_length', 63)
         super(PlainLocationField, self).__init__(*args, **kwargs)
-
-        kwargs.pop('based_fields', None)
-        kwargs.pop('zoom', None)
-        kwargs.pop('suffix', None)
-
-        CharField.__init__(self, max_length=max_length, *args, **kwargs)
